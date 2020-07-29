b2ed23e6557a1e6b856433eb2fa66fd9ed283e02
Amritpal Singh <singhamritpal49@gmail.com>
2020-07-28 23:56:17 -0400

    Updates

---
+++ precache-manifest.d8fa5f036024529f482d400d0ec9c9aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2fb48b29f0c02f93f5f602286dc4d6",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "3a3f922c44dc6115941a",
+    "url": "/covidtracker/static/css/main.88e71906.chunk.css"
+  },
+  {
+    "revision": "e169f4cbc0d71b8bf64b",
+    "url": "/covidtracker/static/js/2.8bb8ce4e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.8bb8ce4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3f922c44dc6115941a",
+    "url": "/covidtracker/static/js/main.b3add63d.chunk.js"
+  },
+  {
+    "revision": "764c85834243dd5b6c1e",
+    "url": "/covidtracker/static/js/runtime-main.74a45620.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
